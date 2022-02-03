0898ff93a0de23aa1c2754ea90462d775a89a8f6
Facundo Vergara <fakumax@gmail.com>
2022-02-02 22:31:08 -0300

    Upgrade Package and Config

--- README.md
+++ README.md
@@ -3,16 +3,40 @@
 This repository has:
 
 _Create a new Next App with Typescript_
+
+```sh
 yarn create next-app --typescript
+```
 
 _Add ChakraUI Typescript_
+
+```sh
 yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^5
 
+```
+
 _Add Dotenv_
+
+```sh
 yarn add dotenv
+```
 
 _Add Mapbox GL_
-yarn add mapbox-gl
 
-_Input to search location_
+```sh
+yarn add mapbox-gl
+```
+
+Removed
+
+<!-- _Input to search location_
+
+```sh
 yarn add @mapbox/mapbox-gl-geocoder
+``` -->
+
+_Input to search location in react_
+
+```sh
+react-map-gl-geocoder
+```
--- src/components/mapbox/mapbox.tsx
+++ src/components/mapbox/mapbox.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { Container } from '@chakra-ui/react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import React, { useState, useRef, useCallback } from 'react';
+import MapGL from 'react-map-gl';
+import Geocoder from 'react-map-gl-geocoder';
+
+const MAPBOX_TOKEN = `${process.env.NEXT_PUBLIC_MAPBOX}`;
 
 const mapbox = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback((newViewport) => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return handleViewportChange({
+      ...newViewport,
+      ...geocoderDefaultOverrides,
+    });
+  }, []);
+
   return (
-    <>
-      <Container maxW="container.lg" centerContent>
-        There are many benefits to a joint design and development system. Not
-        only does it bring benefits to the design team.
-      </Container>
-    </>
+    <div style={{ height: '500px', width: '500px' }}>
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+      </MapGL>
+    </div>
   );
 };
 
