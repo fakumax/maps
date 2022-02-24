1ea5232868926b4d5caa31cf771e51323755c4a6
Facundo Vergara <fakumax@gmail.com>
2022-02-23 22:57:09 -0300

    Add path

--- src/components/Map/MapV2.tsx
+++ src/components/Map/MapV2.tsx
@@ -57,7 +57,116 @@ const MapV2 = () => {
     // only want to work with the map after it has fully loaded
     // if you try to add sources and layers before the map has loaded
     // things will not work properly
+
     map.on('load', () => {
+      /************** HEATMAP ***********/
+      map.addSource('trees', {
+        type: 'geojson',
+        data: '@utils/features.geojson',
+      });
+
+      map.addLayer(
+        {
+          id: 'trees-heat',
+          type: 'heatmap',
+          source: 'trees',
+          maxzoom: 15,
+          paint: {
+            // increase weight as diameter breast height increases
+            'heatmap-weight': {
+              property: 'dbh',
+              type: 'exponential',
+              stops: [
+                [1, 0],
+                [62, 1],
+              ],
+            },
+            // increase intensity as zoom level increases
+            'heatmap-intensity': {
+              stops: [
+                [11, 1],
+                [15, 3],
+              ],
+            },
+            // use sequential color palette to use exponentially as the weight increases
+            'heatmap-color': [
+              'interpolate',
+              ['linear'],
+              ['heatmap-density'],
+              0,
+              'rgba(236,222,239,0)',
+              0.2,
+              'rgb(208,209,230)',
+              0.4,
+              'rgb(166,189,219)',
+              0.6,
+              'rgb(103,169,207)',
+              0.8,
+              'rgb(28,144,153)',
+            ],
+            // increase radius as zoom increases
+            'heatmap-radius': {
+              stops: [
+                [11, 15],
+                [15, 20],
+              ],
+            },
+            // decrease opacity to transition into the circle layer
+            'heatmap-opacity': {
+              default: 1,
+              stops: [
+                [14, 1],
+                [15, 0],
+              ],
+            },
+          },
+        },
+        'waterway-label'
+      );
+      map.addLayer(
+        {
+          id: 'trees-point',
+          type: 'circle',
+          source: 'trees',
+          minzoom: 14,
+          paint: {
+            // increase the radius of the circle as the zoom level and dbh value increases
+            'circle-radius': {
+              property: 'dbh',
+              type: 'exponential',
+              stops: [
+                [{ zoom: 15, value: 1 }, 5],
+                [{ zoom: 15, value: 62 }, 10],
+                [{ zoom: 22, value: 1 }, 20],
+                [{ zoom: 22, value: 62 }, 50],
+              ],
+            },
+            'circle-color': {
+              property: 'dbh',
+              type: 'exponential',
+              stops: [
+                [0, 'rgba(236,222,239,0)'],
+                [10, 'rgb(236,222,239)'],
+                [20, 'rgb(208,209,230)'],
+                [30, 'rgb(166,189,219)'],
+                [40, 'rgb(103,169,207)'],
+                [50, 'rgb(28,144,153)'],
+                [60, 'rgb(1,108,89)'],
+              ],
+            },
+            'circle-stroke-color': 'white',
+            'circle-stroke-width': 1,
+            'circle-opacity': {
+              stops: [
+                [14, 0],
+                [15, 1],
+              ],
+            },
+          },
+        },
+        'waterway-label'
+      );
+      /************** HEATMAP ***********/
       // Add a geojson point source.
       // Heatmap layers also work with a vector tile source.
       map.addSource('earthquakes', {
@@ -266,7 +375,9 @@ const MapV2 = () => {
         },
       });
     });
-
+    map.on('click', 'trees-point', (event) => {
+      new mapboxgl.Popup().setLngLat(event.features[0].geometry.coordinates).setHTML(`<strong>DBH:</strong> ${event.features[0].properties.dbh}`).addTo(map);
+    });
     // When a click event occurs on a feature in the states layer,
     // open a popup at the location of the click, with description
     // HTML from the click event's properties.
